19b9da7dcbb28a5842be2a74904dd78696b321d5
LucaLogallo <lucalogallo1998@gmail.com>
2021-05-03 16:27:13 +0200

    errore

---
+++ assets/js/script9.js
@@ -0,0 +1,48 @@
+$(()=>{
+  const arrAnimali = [
+    
+      { nome: "leone", famiglia: "felidi", classe: "mammiferi" },
+	    { nome: "cane", famiglia: "canidi", classe: "mammiferi" },
+	    { nome: "gallina", famiglia: "fasianidi", classe: "uccelli" },
+
+  ];
+  let addAnimal;
+  do{
+    addAnimal = prompt("vuoi inseire altri animali alla lista già presente? rispondi con si oppure no");
+  }while(addAnimal !== "si" && addAnimal !== "Si" && addAnimal !== "SI" && addAnimal !== "sI" && addAnimal !== "no" && addAnimal !== "No" && addAnimal !== "nO");
+
+  if(addAnimal === "si" || addAnimal === "SI" || addAnimal === "sI"){
+    let numeroAnimaliAdd = 0;
+    do{
+      numeroAnimaliAdd = parseInt(prompt("inserisci il numero di animali che vuoi aggiungere"));
+    }while(numeroAnimaliAdd < 1);
+
+    for(var i = 0; i < numeroAnimaliAdd; i++){
+      let nome = prompt("inserisci il nome dell'animale");
+      let famiglia = prompt("inserisci la famiglia");
+      let classe = prompt("inserisci la classe dell'animale");
+      let animale ={
+        nome: nome,
+        famiglia:famiglia,
+        classe:classe
+      }
+      arrAnimali.push(animale);
+    }
+
+    arrAnimali.forEach((animale)=>{
+      console.log(animale);
+    });
+
+    const mammiferi = arrAnimali.filter((animale)=>{animale.classe ==='mammiferi'});
+    mammiferi.forEach((animale)=>{
+      console.log(animale);
+    });
+  }else{
+
+    const mammiferi = arrAnimali.filter((animale)=>{animale.classe==='mammiferi'});
+
+    mammiferi.forEach((animale)=>{
+      console.log(animale);
+    });
+  }
+});
